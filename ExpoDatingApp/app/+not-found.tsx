5ed9be6442703c6df8d8003dcaf6b95cb5b1085c
Jesse Varkul <jessevarkul@gmail.com>
2025-07-06 19:07:25 -0400

    Expo tutorial app has been completed.

--- ExpoDatingApp/app/+not-found.tsx
+++ ExpoDatingApp/app/+not-found.tsx
@@ -1,32 +1,32 @@
+import {View, StyleSheet} from 'react-native';
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+export default function NotFoundScreen(){
+    return(
+        <>
+            <Stack.Screen options={{ title: 'Opps! Not Found'}}/>
+            <View style={styles.container}>
+                <Link href="/" style={styles.button}>
+                    Go back to Home Screen!
+                </Link>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#25292e',
+    },
+    text: {
+        color: '#fff',
+    },
+    button: {
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        color: '#fff',
+    },
 });
